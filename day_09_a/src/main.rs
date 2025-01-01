045b6c7a0952616b319873f65ae0d38ffb671ff8
torkel <torkel@voysys.se>
2025-01-01 21:40:57 +0100

    9a

---
+++ day_09_a/src/main.rs
@@ -0,0 +1,58 @@
+use std::{error::Error, time::Instant};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let t = Instant::now();
+
+    let input = include_str!("../input");
+
+    let mut res = 0;
+
+    for line in input.lines() {
+        let n_0: Vec<i64> = line
+            .split_ascii_whitespace()
+            .map(|v| v.parse::<i64>().unwrap())
+            .collect();
+
+        let mut nums = vec![n_0];
+
+        let mut all_zeroes = false;
+        while !all_zeroes {
+            let prev = nums.last().unwrap();
+
+            let mut n_i = Vec::new();
+
+            all_zeroes = true;
+
+            for p in prev.windows(2) {
+                let v = p[1] - p[0];
+                if v != 0 {
+                    all_zeroes = false;
+                }
+                n_i.push(v);
+            }
+
+            nums.push(n_i);
+        }
+
+        let mut level = nums.len() - 1;
+        loop {
+            if level == nums.len() - 1 {
+                nums[level].push(0);
+            } else {
+                let last_below = *nums[level + 1].last().unwrap();
+                let last_on_level = *nums[level].last().unwrap();
+                nums[level].push(last_below + last_on_level);
+            }
+
+            if level == 0 {
+                res += nums[level].last().unwrap();
+                break;
+            }
+            level -= 1;
+        }
+    }
+
+    println!("res: {res}, {} us", t.elapsed().as_micros());
+
+    Ok(())
+}
